557d3b1c81fb4e299ec1a8cef6119906af4b6578
ara <joarario92@gmail.com>
2020-11-21 07:11:26 +0900

    Studying map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const foodILike = [
+  {
+    name: 'kimchi',
+    price: 9000
+  },
+  {
+    name: 'ramen',
+    price: 4500
+  },
+  {
+    name: 'pizza',
+    price: 21900
+  }
+];
+
+foodILike.map(dish => {
+  console.log(dish.name);
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( // 반환값이 많아지면 소괄호 감싸야함. 지우지않게 주의
+    <div>
+      <Food />
     </div>
   );
+}
+function Food() {
+	return <h3> sdfsdf </h3>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+ReactDOM.render( // react 렌더 (책 표현 : 그린다고 생각하면 돼)
+  <App />, // APP 컴포넌트를 (JSX요소는 반드시 하나의 태그로 감싸야한다)
+  document.getElementById('root') // index.html의 아이디가 'root'인 엘리먼트에
 );
-
-reportWebVitals();
