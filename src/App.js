62aae20734084f3bbe999a6b8adf3bdb6205a0b2
ara <joarario92@gmail.com>
2020-11-22 19:12:55 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
 import React from 'react';
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'kimchi',
-    price: 9000,
-    ratind: 5
-  },
-  {
-    id: 2,
-    name: 'ramen',
-    price: 4500,
-    ratind: 3
-  },
-  {
-    id: 3,
-    name: 'pizza',
-    price: 21900,
-    ratind: 4
-  }
-];
+class App extends React.Component {
+  state = { // 반드시 컴포넌트 안에서 소문자 형태
+    count: 0
+  };
 
-function App() {
-  return (
-    <div>
-      {
-        foodILike.map( (dish) => (
-          <Food name={dish.name} price={dish.price} key={dish.id} />
-        ))
-      }
-    </div>
-  );
-}
-function Food({ name, price }) {
-	return (
-    <div>
-      <h3>I like {name}</h3>
-      <p>price : {price}</p>
-    </div>
-  );
+  add = () => {
+    this.setState( current => ({
+      count: current.count +1,
+    }));
+  };
+
+  minus = () => {
+    this.setState({ count: this.state.count-1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
